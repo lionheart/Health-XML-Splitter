1d1198f8b2abd1be82d7d42e461daabbed2c495e
Dan Loewenherz <dan@lionheartsw.com>
2022-09-19 08:07:12 -0500

    update pod TARGET_DIRECTORY support files

--- Pods/Target Support Files/Pods-Health Data Importer XML Splitter/Pods-Health Data Importer XML Splitter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Health_Data_Importer_XML_Splitter : NSObject
-@end
-@implementation PodsDummy_Pods_Health_Data_Importer_XML_Splitter
-@end
